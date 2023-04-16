83236c42239967cf6a6939624efb6dbfebc77985
smitasen <smitasen1508@gmail.com>
2023-04-16 17:36:21 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
